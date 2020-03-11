fdd9dfcde185bc5489d85051d22d9efd87c40c1d
goncalog <goncalogordo@gmail.com>
2020-03-11 10:27:15 +0000

    Add displayController module

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -22,4 +22,31 @@ const Player = (name, mark) => {
     }
 
     return {getName, getMark};
-} 
+}
+
+const displayController = (() => {
+    let lastPlayer;
+    let nextPlayer;
+
+    const getLastPlayer = () => {
+        return lastPlayer;
+    }
+    
+    const getNextPlayer = () => {
+        return nextPlayer;
+    }
+
+    const setLastPlayer = (player) => {
+        lastPlayer = player;
+    }
+    
+    const setNextPlayer = (player) => {
+        nextPlayer = player;
+    }
+
+    const updateDisplay = (pos, mark) => {
+        // get display and change innerHtml according to mark
+    }
+
+    return {getLastPlayer, getNextPlayer, setLastPlayer, setNextPlayer, updateDisplay}
+})();
